7d5770070f2dc5256155e568196de4de5acc9647
axie22 <alexxie9667@gmail.com>
2026-02-12 14:01:56 -0500

    Add raylib 2d camera

--- src/main.cpp
+++ src/main.cpp
@@ -7,17 +7,26 @@ int main(void) {
     const int screenHeight = 600;
     const int radius = 8.0f;
 
+    // Raylib setup
     InitWindow(screenWidth, screenHeight, "Nebula");
     SetTargetFPS(60);
+    Camera2D camera = { 0 };
+    camera.target = { screenWidth / 2, screenHeight / 2};
+    camera.offset = { screenWidth / 2, screenHeight / 2};
+    camera.rotation = 0.0f;
+    camera.zoom = 1.0f;
+
+    // Simulation setup
     Simulation sim;
     sim.Init();
+    
     while (!WindowShouldClose()) {
         float dt = GetFrameTime();
         sim.Update(dt);
-
+        
         BeginDrawing();
+        BeginMode2D(camera);
         ClearBackground(BLACK);
-
         for (int i = 0; i < sim.nodes.size(); ++i) {
             Color nodeColor = RAYWHITE;
             if (i == sim.draggedNodeId) {
@@ -27,6 +36,7 @@ int main(void) {
         }
 
         DrawFPS(10, 10);
+        EndMode2D();
         EndDrawing();
     }
     CloseWindow();
