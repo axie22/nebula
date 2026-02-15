ad47f118fbe3161ee62162a8a33d2c4ff79f458f
axie22 <alexxie9667@gmail.com>
2026-02-15 15:47:45 -0500

    Modify simulation vars to be public

--- src/Simulation.h
+++ src/Simulation.h
@@ -10,22 +10,26 @@ class Simulation {
     public:
         std::vector<Node> nodes;
         std::vector<Edge> edges;
+
         int draggedNodeId = -1;
+        static constexpr int NUMBER_OF_NODES = 10;
+        float REPULSION_FORCE = 2000.0f;
+        static constexpr float TARGET_LENGTH = 150.0f; // We want connected nodes to be this far apart
+        static constexpr float SPRING_K = 0.02f;       // Stiffness (how hard it pulls)
 
         void Init();
         void Update(float dt, Vector2 worldMouse);
+
+        
 
     private:
         Vector2 center = { 400.0f, 300.0f };
         float CalculateNodeDistance(Node p1, Node p2);
         float CalculateMouseDistance(Vector2 mouse, Node p1);
-        const float SPAWN_LOWER = -10.0f; 
-        const float SPAWN_UPPER = 10.0f;
-        const int NUMBER_OF_NODES = 10;
-        const int NODE_RADIUS = 8.0;
-        const float REPULSION_FORCE = 2000.0f;
-        const float TARGET_LENGTH = 150.0f; // We want connected nodes to be this far apart
-        const float SPRING_K = 0.02f;       // Stiffness (how hard it pulls)
+        static constexpr float SPAWN_LOWER = -10.0f; 
+        static constexpr float SPAWN_UPPER = 10.0f;
+        
+        static constexpr int NODE_RADIUS = 8.0;
 
         void CheckClickedNode(Vector2 worldMouse);
 };
